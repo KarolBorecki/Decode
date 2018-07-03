20aa07dd5bb90ce6004f6a07a01d5e865db6c42c
Karol <karol.boreck@gmail.com>
2018-07-04 00:49:00 +0200

    Updated graphic, added first elements to playscreen

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
@@ -9,22 +11,36 @@ Window.size = (400, 700)
 Builder.load_file('graphic.kv')
 
 
+class Block(Image):
+    def __init__(self, c, **kwargs):
+        super(Block, self).__init__(**kwargs)
+        self.c = c
+        self.source = self.load_source()
+
+    def load_source(self):
+        return "img/blocks/" + str(self.c) + "_block.png"
+
+
+class SquareButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(SquareButton, self).__init__(**kwargs)
+
+
 class MenuScreen(Screen):
     high_score = 23453
 
     light_color = (.8, .8, .8, 1)
     dark_color = (.1, .1, .1, 1)
 
-    def __init__(self, color, **kwargs):
+    def __init__(self, **kwargs):
         super(MenuScreen, self).__init__(**kwargs)
-        self.color = color
+        self.color = True
 
-        self.set_color(color)
+        self.set_color(self.color)
 
     def change_theme(self):
         self.color = not self.color
         self.set_color(self.color)
-        print("CHANGING")
 
     def set_color(self, color):
         if color:
@@ -43,13 +59,8 @@ class SettingsScreen(Screen):
         super(SettingsScreen, self).__init__(**kwargs)
 
 
-class SquareButton(ButtonBehavior, Image):
-    def __init__(self, **kwargs):
-        super(SquareButton, self).__init__(**kwargs)
-
-
 sm = ScreenManager()
-sm.add_widget(MenuScreen(True, name='menu'))
+sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(PlayScreen(name='play'))
 sm.add_widget(SettingsScreen(name='settings'))
 
