08214b3e3b7ba2975456c71c63c8c6dd35566cfb
Karol <karol.boreck@gmail.com>
2018-07-10 14:32:33 +0200

    Added blocks fall, points now added after block destruction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.behaviors import ButtonBehavior
@@ -17,14 +18,125 @@ class Block(ButtonBehavior, Image):
         self.index_x = x
         self.index_y = y
         self.c = c
-        self.source = self.load_source()
+        self.load_source()
 
     def on_press(self):
         self.parent.parent.parent.block_pressed(self)
 
+    def look_for_line(self):
+        if self.check_vertical() or self.check_horizontal():
+            return True
+        return False
+
+    def check_vertical(self):
+        parent = self.parent.parent.parent
+        y = self.index_y
+        x = self.index_x
+
+        if not y >= parent.board_size - 1 and not y <= 0:
+            if self.c == parent.blocks[y - 1][x].c and self.c == parent.blocks[y + 1][x].c:
+                if y > 1:
+                    if self.c == parent.blocks[y - 2][x].c:
+                        parent.blocks[y - 2][x].destroy()
+                        parent.add_bomb()
+                if not y >= parent.board_size - 2:
+                    if self.c == parent.blocks[y + 2][x].c:
+                        parent.blocks[y + 2][x].destroy()
+                        parent.add_bomb()
+                parent.blocks[y - 1][x].destroy()
+                parent.blocks[y + 1][x].destroy()
+                self.destroy()
+                return True
+        if y > 1:
+            if self.c == parent.blocks[y - 1][x].c and self.c == parent.blocks[y - 2][x].c:
+                parent.blocks[y - 2][x].destroy()
+                parent.blocks[y - 1][x].destroy()
+                self.destroy()
+                return True
+
+        if not y >= parent.board_size - 2:
+                if self.c == parent.blocks[y + 1][x].c and self.c == parent.blocks[y + 2][x].c:
+                    parent.blocks[y + 1][x].destroy()
+                    parent.blocks[y + 2][x].destroy()
+                    self.destroy()
+                    return True
+        return False
+
+    def check_horizontal(self):
+        parent = self.parent.parent.parent
+        y = self.index_y
+        x = self.index_x
+
+        if not x >= parent.board_size - 1 and not x <= 0:
+            if self.c == parent.blocks[y][x - 1].c and self.c == parent.blocks[y][x + 1].c:
+                if x > 1:
+                    if self.c == parent.blocks[y][x - 2].c:
+                        parent.blocks[y][x - 2].destroy()
+                        parent.add_bomb()
+                if not x >= parent.board_size - 2:
+                    if self.c == parent.blocks[y][x + 2].c:
+                        parent.blocks[y][x + 2].destroy()
+                        parent.add_bomb()
+                parent.blocks[y][x - 1].destroy()
+                self.destroy()
+                parent.blocks[y][x + 1].destroy()
+                return True
+        if not x <= 0:
+            if self.c == parent.blocks[y][x - 1].c and self.c == parent.blocks[y][x - 2].c:
+                self.destroy()
+                parent.blocks[y][x - 1].destroy()
+                parent.blocks[y][x - 2].destroy()
+                return True
+        if not x >= parent.board_size - 2:
+            if self.c == parent.blocks[y][x + 1].c and self.c == parent.blocks[y][x + 2].c:
+                parent.blocks[y][x + 1].destroy()
+                parent.blocks[y][x + 2].destroy()
+                self.destroy()
+                return True
+        return False
+
+    def fall(self, dt):
+        parent = self.parent.parent.parent
+        block_above = parent.blocks[self.index_y - 1][self.index_x]
+        if self.index_y > 0:
+            self.swap_colors(block_above, False)
+            Clock.schedule_once(block_above.fall, 0.0)
+        else:
+            self.check_is_destroyed()
+
+    def destroy(self):
+        Clock.schedule_once(self.set_to_destroyed, 0.5)
+
+    def set_to_destroyed(self, dt):
+        self.set_color("white")
+        Clock.schedule_once(self.fall, 0.05)
+        self.parent.parent.parent.add_score(10)
+
     def move(self, direction):
         swap_block = self.get_block_by_direction(direction)
         self.swap_colors(swap_block)
+        self.parent.parent.parent.last_touched_block = None
+
+    def swap_colors(self, swap_block, look=True):
+        swap_block_color = swap_block.c
+        swap_block.set_color(self.c)
+        self.set_color(swap_block_color)
+        if look:
+            if not swap_block.look_for_line() and not self.look_for_line():
+                self.swap_colors(swap_block, False)
+        self.check_is_destroyed()
+
+    def check_is_destroyed(self):
+        if self.c == "white":
+            Clock.schedule_once(self.randomize_color, 0.5)
+
+    def randomize_color(self, dt):
+        parent = self.parent.parent.parent
+        self.set_color(random.choice(parent.colors))
+
+    def set_color(self, c):
+        self.c = c
+        self.load_source()
 
     def get_block_by_direction(self, direction):
         parent = self.parent.parent.parent
@@ -42,17 +154,11 @@ class Block(ButtonBehavior, Image):
 
         return parent.blocks[swap_block_y][swap_block_x]
 
-    def swap_colors(self, swap_block):
-        swap_block_color = swap_block.c
-        swap_block.set_color(self.c)
-        self.set_color(swap_block_color)
-
-    def set_color(self, c):
-        self.c = c
-        self.source = self.load_source()
+    def print_index(self):
+        print("y: " + str(self.index_y) + " x: " + str(self.index_x))
 
     def load_source(self):
-        return "img/blocks/" + str(self.c) + "_block.png"
+        self.source = "img/blocks/" + str(self.c) + "_block.png"
 
 
 class SquareButton(ButtonBehavior, Image):
@@ -89,7 +195,8 @@ class PlayScreen(Screen):
     touch_accuracy = 30
     board_size = 8
 
-    bombs_count = 4
+    score = 0
+    bombs_count = 0
 
     last_x = 0
     last_y = 0
@@ -112,6 +219,14 @@ class PlayScreen(Screen):
     def bomb_drag():
         print("BOMB DRAGGED")
 
+    def add_score(self, amount):
+        self.score += amount
+        self.score_board.text = str(self.score)
+
+    def add_bomb(self):
+        self.bombs_count += 1
+        self.bomb_label.text = str(self.bombs_count)
+
     def block_pressed(self, block):
         self.last_touched_block = block
 
@@ -122,24 +237,25 @@ class PlayScreen(Screen):
         super(PlayScreen, self).on_touch_down(touch)
 
     def on_touch_up(self, touch):
-        self.actually_x = touch.pos[0]
-        self.actually_y = touch.pos[1]
-        dif_x = self.actually_x - self.last_x
-        dif_y = self.actually_y - self.last_y
+        if self.last_touched_block is not None:
+            self.actually_x = touch.pos[0]
+            self.actually_y = touch.pos[1]
+            dif_x = self.actually_x - self.last_x
+            dif_y = self.actually_y - self.last_y
 
-        direction = ""
-        if dif_y > self.touch_accuracy:
-            direction = "up"
-        elif dif_y < -self.touch_accuracy:
-            direction = "down"
-        elif dif_x > self.touch_accuracy:
-            direction = "right"
-        elif dif_x < -self.touch_accuracy:
-            direction = "left"
-        else:
-            return
+            direction = ""
+            if dif_y > self.touch_accuracy:
+                direction = "up"
+            elif dif_y < -self.touch_accuracy:
+                direction = "down"
+            elif dif_x > self.touch_accuracy:
+                direction = "right"
+            elif dif_x < -self.touch_accuracy:
+                direction = "left"
+            else:
+                return
 
-        self.last_touched_block.move(direction)
+            self.last_touched_block.move(direction)
         super(PlayScreen, self).on_touch_up(touch)
 
 
@@ -154,11 +270,10 @@ sm.add_widget(PlayScreen(name='play'))
 sm.add_widget(SettingsScreen(name='settings'))
 
 
-class TestApp(App):
-
+class Decode(App):
     def build(self):
         return sm
 
 
 if __name__ == '__main__':
-    TestApp().run()
+    Decode().run()
